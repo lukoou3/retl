17d990d4753d2deea53e130fa86a16be3ce98ddf
lifengchao <lifengchao@geedgenetworks.com>
2025-04-24 17:12:32 +0800

    配置feature，默认不编译mysql和postgres

--- src/connector/mod.rs
+++ src/connector/mod.rs
@@ -2,11 +2,16 @@ pub mod faker;
 pub mod source;
 pub mod sink;
 pub mod print;
+#[cfg(feature = "kafka")]
 pub mod kafka;
+#[cfg(feature = "starrocks")]
 pub mod starrocks;
 pub mod batch;
+#[cfg(feature = "clickhouse")]
 pub mod clickhouse;
+#[cfg(feature = "mysql")]
 mod mysql;
+#[cfg(feature = "postgres")]
 mod postgres;
 mod inline;
 
